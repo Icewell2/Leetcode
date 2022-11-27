49782ec7c5c85eaa709d8eab1be6d326a0c5bd5d
Icewell-PG <57959177+Icewell2@users.noreply.github.com>
2022-11-27 16:11:31 -0500

    Case when and multi-Join

---
+++ SQL/1440_Evaluate_Boolean_Expression.sql
@@ -0,0 +1,112 @@
+SQL Schema
+
+Create Table If Not Exists Variables (name varchar(3), value int)
+Create Table If Not Exists Expressions (left_operand varchar(3), operator ENUM('>', '<', '='), right_operand varchar(3))
+Truncate table Variables
+insert into Variables (name, value) values ('x', '66')
+insert into Variables (name, value) values ('y', '77')
+Truncate table Expressions
+insert into Expressions (left_operand, operator, right_operand) values ('x', '>', 'y')
+insert into Expressions (left_operand, operator, right_operand) values ('x', '<', 'y')
+insert into Expressions (left_operand, operator, right_operand) values ('x', '=', 'y')
+insert into Expressions (left_operand, operator, right_operand) values ('y', '>', 'x')
+insert into Expressions (left_operand, operator, right_operand) values ('y', '<', 'x')
+insert into Expressions (left_operand, operator, right_operand) values ('x', '=', 'x')
+
+Table Variables:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| name          | varchar |
+| value         | int     |
++---------------+---------+
+name is the primary key for this table.
+This table contains the stored variables and their values.
+
+ 
+
+Table Expressions:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| left_operand  | varchar |
+| operator      | enum    |
+| right_operand | varchar |
++---------------+---------+
+(left_operand, operator, right_operand) is the primary key for this table.
+This table contains a boolean expression that should be evaluated.
+operator is an enum that takes one of the values ('<', '>', '=')
+The values of left_operand and right_operand are guaranteed to be in the Variables table.
+
+ 
+
+Write an SQL query to evaluate the boolean expressions in Expressions table.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Variables table:
++------+-------+
+| name | value |
++------+-------+
+| x    | 66    |
+| y    | 77    |
++------+-------+
+Expressions table:
++--------------+----------+---------------+
+| left_operand | operator | right_operand |
++--------------+----------+---------------+
+| x            | >        | y             |
+| x            | <        | y             |
+| x            | =        | y             |
+| y            | >        | x             |
+| y            | <        | x             |
+| x            | =        | x             |
++--------------+----------+---------------+
+Output: 
++--------------+----------+---------------+-------+
+| left_operand | operator | right_operand | value |
++--------------+----------+---------------+-------+
+| x            | >        | y             | false |
+| x            | <        | y             | true  |
+| x            | =        | y             | false |
+| y            | >        | x             | true  |
+| y            | <        | x             | false |
+| x            | =        | x             | true  |
++--------------+----------+---------------+-------+
+Explanation: 
+As shown, you need to find the value of each boolean expression in the table using the variables table.
+
+
+
+Sol:
+
+# Write your MySQL query statement below
+
+SELECT 
+    left_operand,
+    operator,
+    right_operand,
+    (CASE 
+        WHEN operator = '=' AND v1.value = v2.value THEN 'true'
+        WHEN operator = '>' AND v1.value > v2.value THEN 'true'
+        WHEN operator = '<' AND v1.value < v2.value THEN 'true'
+        ELSE 'false'
+    END) AS value
+FROM Expressions
+JOIN Variables v1
+    ON Expressions.left_operand = v1.name
+JOIN Variables v2
+    ON Expressions.right_operand = v2.name
+;
+
+
+#It's important to keep in mind how to match two values when use JOIN
