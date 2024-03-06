02d1f11660fefc52de46e2089c1e039bc9f94674
Icewell-PG <57959177+Icewell2@users.noreply.github.com>
2024-03-05 21:43:41 -0500

    locate column and row, loc iloc

---
+++ Python/177_Nth_Highest_Salary.py
@@ -0,0 +1,68 @@
+# Table: Employee
+
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | id          | int  |
+# | salary      | int  |
+# +-------------+------+
+# id is the primary key (column with unique values) for this table.
+# Each row of this table contains information about the salary of an employee.
+
+ 
+
+# Write a solution to find the nth highest salary from the Employee table. If there is no nth highest salary, return null.
+
+# The result format is in the following example.
+
+ 
+
+# Example 1:
+
+# Input: 
+# Employee table:
+# +----+--------+
+# | id | salary |
+# +----+--------+
+# | 1  | 100    |
+# | 2  | 200    |
+# | 3  | 300    |
+# +----+--------+
+# n = 2
+# Output: 
+# +------------------------+
+# | getNthHighestSalary(2) |
+# +------------------------+
+# | 200                    |
+# +------------------------+
+
+# Example 2:
+
+# Input: 
+# Employee table:
+# +----+--------+
+# | id | salary |
+# +----+--------+
+# | 1  | 100    |
+# +----+--------+
+# n = 2
+# Output: 
+# +------------------------+
+# | getNthHighestSalary(2) |
+# +------------------------+
+# | null                   |
+# +------------------------+
+
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    employee = employee['salary'].drop_duplicates()
+    employee = employee.sort_values(ascending = False)
+    result = pd.DataFrame(columns = ['getNthHighestSalary(' + str(N) + ')' ])
+    if N > employee.shape[0] or N <= 0:
+        result.loc[0] = None
+        return result
+    else:
+        print(employee)
+        result.loc[0] = (employee.iloc[N-1])
+        return result
